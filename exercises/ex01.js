7206ac67f1ccc05ea4a49ee71801d42dd37a8cff
yk-graph <yokokura.tatsuya@gmail.com>
2026-01-27 11:53:03 -0800

    add: Exercise01

--- exercises/ex01.js
+++ exercises/ex01.js
@@ -6,13 +6,32 @@ Instruction
 Create a function named sumLargestNumbers that will receive an array of numbers and return the sum of the two largest numbers in that array.
 */
 
+const ErrorMessages = {
+  NO_INPUT: 'Please input an array',
+  NOT_ARRAY: 'Input must be an array',
+  INSUFFICIENT_ELEMENTS: 'Input array must have at least two elements',
+};
+
 const sumLargestNumbers = function (data) {
-  // Put your solution here
-  return 11;
+  if (data === undefined || data === null) {
+    return ErrorMessages.NO_INPUT;
+  }
+
+  if (!Array.isArray(data)) {
+    return ErrorMessages.NOT_ARRAY;
+  }
+
+  if (data.length < 2) {
+    return ErrorMessages.INSUFFICIENT_ELEMENTS;
+  }
+
+  const sortedData = data.sort((a, b) => b - a);
+  return sortedData[0] + sortedData[1];
 };
 
 console.log(sumLargestNumbers([1, 10])); // 11
 console.log(sumLargestNumbers([1, 2, 3])); // 5
 console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2])); // 126
 
-module.exports = sumLargestNumbers;
+module.exports.sumLargestNumbers = sumLargestNumbers;
+module.exports.ErrorMessages = ErrorMessages;
--- tests/ex01.test.js
+++ tests/ex01.test.js
@@ -1,4 +1,4 @@
-const sumLargestNumbers = require("../exercises/ex01");
+const { sumLargestNumbers, ErrorMessages } = require('../exercises/ex01');
 
 test("adds 2 largest numbers from array of 2 elements", () => {
   expect(sumLargestNumbers([1, 10])).toBe(11);
@@ -13,3 +13,19 @@ test("adds 2 largest numbers from array of multiple elements", () => {
 test("adds 2 largest numbers from array of multiple negative elements", () => {
   expect(sumLargestNumbers([-10, -4, -34, -6, -92, -2])).toBe(-6);
 });
+
+test("returns error message when input is missing", () => {
+  expect(sumLargestNumbers()).toBe(ErrorMessages.NO_INPUT);
+});
+
+test("returns error message when input is not an array", () => {
+  expect(sumLargestNumbers('')).toBe(ErrorMessages.NOT_ARRAY);
+  expect(sumLargestNumbers('aaa')).toBe(ErrorMessages.NOT_ARRAY);
+  expect(sumLargestNumbers(0)).toBe(ErrorMessages.NOT_ARRAY);
+  expect(sumLargestNumbers(5)).toBe(ErrorMessages.NOT_ARRAY);
+});
+
+test("returns error message when input array has less than two elements", () => {
+  expect(sumLargestNumbers([5])).toBe(ErrorMessages.INSUFFICIENT_ELEMENTS);
+  expect(sumLargestNumbers([])).toBe(ErrorMessages.INSUFFICIENT_ELEMENTS);
+})
