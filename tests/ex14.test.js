12890009ec01c9900d39dce9feb8f24288055915
yk-graph <yokokura.tatsuya@gmail.com>
2026-01-29 11:14:18 -0800

    fix: tests/ex14.test

--- tests/ex14.test.js
+++ tests/ex14.test.js
@@ -1,8 +1,8 @@
-const calculateChange = require("../exercises/ex14");
+const {calculateChange, ErrorMessages} = require("../exercises/ex14");
 
 test("return obj with keys as type of change and amount of each as values, not showing amount = 0", () => {
   expect(calculateChange(1787, 2000)).toStrictEqual({
-    toonie: 1,
+    twoDollar: 1,
     dime: 1,
     penny: 3,
   });
@@ -11,8 +11,8 @@ test("return obj with keys as type of change and amount of each as values, not s
 test("return obj with keys as type of change and amount of each as values, not showing amount = 0", () => {
   expect(calculateChange(2623, 4000)).toStrictEqual({
     tenDollar: 1,
-    toonie: 1,
-    loonie: 1,
+    twoDollar: 1,
+    oneDollar: 1,
     quarter: 3,
     penny: 2,
   });
@@ -20,9 +20,18 @@ test("return obj with keys as type of change and amount of each as values, not s
 
 test("return obj with keys as type of change and amount of each as values, not showing amount = 0", () => {
   expect(calculateChange(501, 1000)).toStrictEqual({
-    toonie: 2,
+    twoDollar: 2,
     quarter: 3,
     dime: 2,
     penny: 4,
   });
 });
+
+test("return 'Just enough cash given, no change due.' when total equals cash", () => {
+  expect(calculateChange(1000, 1000)).toBe(ErrorMessages.NO_CHANGE);
+});
+
+test("return 'Insufficient cash given.' when total is more than cash", () => {
+  expect(calculateChange(1500, 1000)).toBe(ErrorMessages.EXTRA_CASH);
+});
+
